9e151d391639a200bec173c5ebf60e3b7ee820bd
Jonatan Pineda Perez <j.pineda.p1@gmail.com>
2018-10-09 07:29:37 -0600

    Hero model added

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,9 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 
+
 ReactDOM.render(
-  <h1>Hola</h1>,
+  <h1>Dota2</h1>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
